6d613e77115994a172c474c15e79268587dfa074
marceloaugustolealtst-source <marceloaugustolealtst@gmail.com>
2026-09-11 14:03:04 +0100

    Add configuration files and timezones library

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#111827',
+        darker: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+};
